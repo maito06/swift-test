580470b996c83c93ac62bbd46afef784035b7bf0
maito06 <lucazelmans@yahoo.fr>
2016-02-10 18:26:05 +0100

    updated the print
    To let user know who I am

--- SwiftTest/ViewController.swift
+++ SwiftTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello world")
+        print("Hello world it's Luca")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
