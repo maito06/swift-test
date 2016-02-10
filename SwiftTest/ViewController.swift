8bc9128b9c123d1e7aa531cbc6a63ab7eef4e516
LUCA <lucazelmans@yahoo.fr>
2016-02-10 18:28:38 +0100

    I want to be anonymous.

--- SwiftTest/ViewController.swift
+++ SwiftTest/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello world it's Luca")
+        print("Hello world it's me")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
